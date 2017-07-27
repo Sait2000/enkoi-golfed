c04abbf8fb748d7c5fc4fdff3068e5e983983257
Kim Changdong <best564@naver.com>
2017-07-27 20:28:55 +0900

    Use a while statement for the second loop

--- enkoi_universal_py3.py
+++ enkoi_universal_py3.py
@@ -1,5 +1,5 @@
 I=lambda:map(int,input().split());t,n,*_=*I(),0;d=[];m=0
 for M in[max]*t:T,S,P,*_=*I(),0,1;d+=T,;m+=M(T-S,0)*P
-s=m
-for i in range(n-t):s-=d[i%t];v,=I();s+=v;d[i%t]=v;m=M(m,s)
+s=m;i=t
+while i<n:s-=d[i%t];v,=I();s+=v;d[i%t]=v;m=M(m,s);i+=1
 print(m)
